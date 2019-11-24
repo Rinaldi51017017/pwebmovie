dae073b1ce0602c3b51391782c75f15ea7d2791d
rinaldi51017017 <rinaldiadriansyah50@gmail.com>
2019-11-24 23:50:29 +0800

    firsh time

--- js/tmdb.js
+++ js/tmdb.js
@@ -1,2 +1,32 @@
-const key = 'def35b237f416852334d9900f01bc2bf'; //sesuai API Key Anda
-const endpoint_url = 'https://api.themoviedb.org/3/';
+const key = '608ce5e628b4dc66eea24ea292eb79b2';
+const endpoint_url = 'https://api.themoviedb.org/3';
+
+function getListMovie(services, sectiontitle) {
+  fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
+  .then(status)
+  .then(json)
+  .then(function(data) {
+
+    var moviesHTML = "";
+    data.results.forEach(function(movie) {
+      moviesHTML +=`
+      <div class="col m3 s6">
+        <div class="card">
+        <a href="./movie.html?id=${movie.id}">
+        <div class="card-image waves-effect waves-block waves-light">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+        </div>
+        </a>
+        <div class="card-content text-center">
+        <strong>${movie.title}</strong>
+        </div>
+        </div>
+        </div>
+        `;
+    });
+      document.getElementById("movie_list").innerHTML = moviesHTML;
+      document.getElementById("section_title").innerHTML = sectiontitle;
+
+  })
+  .catch(error);
+}
